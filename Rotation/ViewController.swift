78d1b719233e000f93d7ab170fd78ef2e73e51af
Chernoev Andrew <chernoev.wt@gmail.com>
2019-05-19 23:20:29 +0700

    Rotate landscape view

--- Rotation/ViewController.swift
+++ Rotation/ViewController.swift
@@ -17,6 +17,26 @@ class ViewController: UIViewController {
         return .allButUpsideDown
     }
     
+    private enum Constants {
+        static let rotationAngle = -90.0 / 180.0 * CGFloat.pi
+        
+        static func translationX(view: UIView) -> CGFloat {
+            return view.frame.size.width / 2.0
+        }
+        
+        static func translationY(view: UIView) -> CGFloat {
+            return view.frame.size.height / 2.0
+        }
+    }
+    
+    fileprivate func transformView() {
+        if let landscapeView = landscapeView {
+            landscapeView.transform = CGAffineTransform(translationX: Constants.translationX(view: landscapeView),
+                                                        y: Constants.translationY(view: landscapeView))
+            landscapeView.transform = CGAffineTransform(rotationAngle: Constants.rotationAngle)
+        }
+    }
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         portraitView?.center = CGPoint(x: view.bounds.midX,
@@ -28,57 +48,63 @@ class ViewController: UIViewController {
     override func viewWillTransition(to size: CGSize,
                                      with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-
-        coordinator.animate(alongsideTransition: { [weak self] (context) in
-            guard let transformedView = self?.portraitView else {
-                return
-            }
-
+        
+        func transition(destination: UIView, context: UIViewControllerTransitionCoordinatorContext) {
             let deltaTransform = coordinator.targetTransform
-            let deltaAngle: Float = atan2f(Float(deltaTransform.b),
-                                           Float(deltaTransform.a))
-            if var currentRotation = transformedView.layer.value(forKeyPath: "transform.rotation.z") as? Double {
-                currentRotation += -1 * Double(deltaAngle) + 0.0001;
-                transformedView.layer.setValue(currentRotation,
-                                                     forKeyPath: "transform.rotation.z")
+            let deltaAngle = atan2(Double(deltaTransform.b),
+                                   Double(deltaTransform.a))
+            if var currentRotation = destination.layer.value(forKeyPath: "transform.rotation.z") as? Double {
+                currentRotation += -1 * deltaAngle + 0.0001;
+                destination.layer.setValue(currentRotation,
+                                               forKeyPath: "transform.rotation.z")
             }
-        }) { [weak self] (context) in
-            guard let transformedView = self?.portraitView else {
-                return
-            }
-            var currentTransform = transformedView.transform
+        }
+        
+        func transitionComplete(destination: UIView, context: UIViewControllerTransitionCoordinatorContext) {
+            var currentTransform = destination.transform
             currentTransform.a = round(currentTransform.a)
             currentTransform.b = round(currentTransform.b)
             currentTransform.c = round(currentTransform.c)
             currentTransform.d = round(currentTransform.d)
-            transformedView.transform = currentTransform
+            destination.transform = currentTransform
+        }
+        
+        coordinator.animate(alongsideTransition: { [weak self] (context) in
+            if let portraitView = self?.portraitView {
+                transition(destination: portraitView, context: context)
+            }
+            
+            if let landscapeView = self?.landscapeView {
+                transition(destination: landscapeView, context: context)
+            }
+        }) { [weak self] (context) in
+            if let portraitView = self?.portraitView {
+                transitionComplete(destination: portraitView, context: context)
+            }
+            
+            if let landscapeView = self?.landscapeView {
+                transitionComplete(destination: landscapeView, context: context)
+            }
         }
     }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        transformView()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
     
     @IBAction func swap(_ sender: Any?) {
-        
-        super.view.sizeToFit()
-        super.view.setNeedsLayout()
-        super.view.setNeedsDisplay()
-        super.view.layoutIfNeeded()
-        super.view.layoutSubviews()
-        
-        
-        view.sizeToFit()
-        view.setNeedsLayout()
-        view.setNeedsDisplay()
-        view.layoutIfNeeded()
-        view.layoutSubviews()
-        
-        
-        if portraitView?.isHidden == true {
-            portraitView?.isHidden = false
-            landscapeView?.isHidden = true
-        } else {
-            portraitView?.isHidden = true
-            landscapeView?.isHidden = false
-        }
+//        if portraitView?.isHidden == true {
+//            portraitView?.isHidden = false
+//            landscapeView?.isHidden = true
+//        } else {
+//            portraitView?.isHidden = true
+//            landscapeView?.isHidden = false
+//        }
     }
     
     @IBAction func close(_ sender: Any?) {
@@ -87,22 +113,16 @@ class ViewController: UIViewController {
 }
 
 
-class ChildPortraitViewController: UIViewController {
-    override var shouldAutorotate: Bool {
-        return false
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .portrait
-    }
+class ChildPortraitViewController: ContentableViewController {
+   
 }
 
-class ChildLandscapeViewController: UIViewController {
-    override var shouldAutorotate: Bool {
-        return false
-    }
+class ChildLandscapeViewController: ContentableViewController {
+    
+}
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .landscape
-    }
+
+class ContentableViewController: UIViewController {
+    @IBOutlet var content: UIView?
+
 }
